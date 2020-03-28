3555a0f855bf42d1c36a1eb80c436b30d5146a16
prakhar agarwal <prakhar.agarwal1_cs18@gla.ac.in>
2020-03-28 18:36:01 +0530

    main

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,30 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
-public class MyMain {
+public class MyMain
+{
+    public static void main(String[] args)
+    {
+        MyBinarySearchTree MyBinarySearchTree = new MyBinarySearchTree();
+        Scanner sc =   new Scanner(System.in);
+
+        int inputs = 7;
+        while (inputs > 0)
+        {
+            MyBinarySearchTree.add(sc.nextInt());
+            inputs--;
+
+        }
+        MyBinarySearchTree.pre(MyBinarySearchTree.root);
+        System.out.println();
+        MyBinarySearchTree.post(MyBinarySearchTree.root);
+
+    }
 }
