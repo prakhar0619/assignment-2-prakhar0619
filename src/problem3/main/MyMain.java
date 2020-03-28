4501cee6896bdccf0f3d47b875fae9758652f83c
prakhar agarwal <prakhar.agarwal1_cs18@gla.ac.in>
2020-03-28 18:40:21 +0530

    main

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,24 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
-public class MyMain {
+public class MyMain
+{
+    public static void main(String[] args)
+    {
+        MyPriorityQueue obj =new MyPriorityQueue();
+        obj.enQueue(new Node(new Student(45, "YoYo")));
+        obj.enQueue(new Node(new Student(6, "Abhishek")));
+        obj.enQueue(new Node(new Student(1, "Prakhar")));
+        obj.enQueue(new Node(new Student(54, "Manish")));
+        obj.enQueue(new Node(new Student(23, "Jayant")));
+        obj.printQueue();
+    }
+
 }
