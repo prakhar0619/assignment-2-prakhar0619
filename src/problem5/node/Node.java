a7838c8c624f735f605348e5efd7cb2f32f6f045
prakhar agarwal <prakhar.agarwal1_cs18@gla.ac.in>
2020-03-28 19:04:25 +0530

    node

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -5,6 +5,37 @@
  *  Time: 7:06 PM
  */
 package problem5.node;
+import problem5.student.Student;
+
 // to define node properties
-public class Node {
+public class Node
+{
+    private Student student;
+    private Node next;
+
+    public Node(Student student)
+    {
+        this.student = student;
+        next = null;
+    }
+
+    public Student getData()
+    {
+        return student;
+    }
+
+    public void setData(Student student)
+    {
+        this.student = student;
+    }
+
+    public Node getNext()
+    {
+        return next;
+    }
+
+    public void setNext(Node next)
+    {
+        this.next = next;
+    }
 }
