c1fd75926df9e458bc32521a2cdcd883fd81243d
prakhar agarwal <prakhar.agarwal1_cs18@gla.ac.in>
2020-03-28 18:45:06 +0530

    node

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -5,6 +5,36 @@
  *  Time: 7:13 PM
  */
 package problem3.node;
+import problem5.student.Student;
 
-public class Node {
+public class Node
+{
+    private Student student;
+    private Node next;
+
+    public Node(Student student)
+    {
+        this.student=student;
+        next = null;
+    }
+
+    public Student getData()
+    {
+        return student;
+    }
+
+    public void setData(Student student)
+    {
+        this.student = student;
+    }
+
+    public Node getNext()
+    {
+        return next;
+    }
+
+    public void setNext(Node next)
+    {
+        this.next = next;
+    }
 }
