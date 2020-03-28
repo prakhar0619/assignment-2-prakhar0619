3e04692731ed64804d06c9f5ac23be7e2b850d3b
prakhar agarwal <prakhar.agarwal1_cs18@gla.ac.in>
2020-03-28 18:49:07 +0530

    my queue

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -5,7 +5,62 @@
  *  Time: 7:11 PM
  */
 package problem4.myqueue;
-// to create queue to store pre - order successor
 
-public class MyQueue {
+import problem1.node.TreeNode;
+
+import static problem1.mybst.MyBinarySearchTree.pre;
+import static problem1.mybst.MyBinarySearchTree.preOrderString;
+
+public class MyQueue
+{
+    private Node rear;
+    private Node front;
+    private int size;
+
+    public MyQueue()
+    {
+        front= null;
+        rear= null;
+        size= 0;
+    }
+
+    public void enqueue(TreeNode root)
+    {
+        pre(root);
+        String[] data = preOrderString.toString().split(" ");
+        for (int i = 0; i < data.length; i++)
+        {
+            Node node = new Node(Integer.parseInt(data[i]));
+            if (front == null)
+            {
+                rear = node;
+                front = node;
+                size++;
+            } else
+                {
+                rear.setNext(node);
+                rear = node;
+                size++;
+            }
+        }
+    }
+
+    public void printQueue()
+    {
+        Node temp = this.front.getNext();
+        for (int i = 0; i < this.size - 1; i++)
+        {
+            if (i != this.size - 2)
+            {
+                System.out.print(temp.getData() + "->");
+                temp = temp.getNext();
+            } else
+                {
+                System.out.println(temp.getData());
+            }
+        }
+    }
 }
+
+
+
