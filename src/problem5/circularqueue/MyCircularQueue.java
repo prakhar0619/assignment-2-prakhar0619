994a58bf162c1c6fc2a6c1da6fbb79ede1b1147e
prakhar agarwal <prakhar.agarwal1_cs18@gla.ac.in>
2020-03-28 18:56:41 +0530

    my circular queue

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -6,5 +6,76 @@
  */
 package problem5.circularqueue;
 //to implement circular queue
-public class MyCircularQueue {
+public class MyCircularQueue
+{
+
+ private Node front;
+    private int size;
+
+    public MyCircularQueue()
+    {
+        front = null;
+        size = 0;
+    }
+
+    public void enqueue(ArrayList<Student> addData)
+    {
+        for (Student stu : addData)
+        {
+            if (stu.getBackLog() - stu.getAppearance() == 0)
+            {
+            }
+            else if (stu.getBackLog() - stu.getAppearance() > 0)
+            {
+
+                Node temp = new Node(stu);
+                if (this.front == null)
+                {
+                    front = temp;
+                    temp.setNext(temp);
+                    size++;
+                } else
+                    {
+                    temp.setNext(this.front);
+                    Node parser = this.front;
+                    while (parser.getNext() != this.front)
+                    {
+                        parser = parser.getNext();
+                    }
+                    parser.setNext(temp);
+                    front = temp;
+                    size++;
+                }
+            } else
+                {
+
+                System.out.println("Data is Invalid");
+            }
+        }
+
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public void printQueue()
+    {
+        Node temp = this.front;
+        for (int i = 0; i < this.size; i++)
+        {
+            if (i != this.size - 1)
+            {
+                System.out.print(temp.getData().toStrings() + ":-");
+                temp = temp.getNext();
+            } else
+                {
+                System.out.println(temp.getData().toStrings());
+            }
+        }
+    }
 }
+
+
+
