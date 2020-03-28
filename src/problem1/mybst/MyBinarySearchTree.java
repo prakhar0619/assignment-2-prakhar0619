7de3e2c7142e295ec9cb2fc1ba2eb7a97859890f
prakhar agarwal <prakhar.agarwal1_cs18@gla.ac.in>
2020-03-28 18:31:23 +0530

    binary search tree

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -2,18 +2,22 @@ package problem1.mybst;
 import problem1.node.TreeNode;
 
 
-public class MyBinarySearchTree {
+public class MyBinarySearchTree
+{
     public static StringBuilder preOrderString;
     public TreeNode root;
     public int c = 0;
 
-    public MyBinarySearchTree() {
+    public MyBinarySearchTree()
+    {
         this.root = null;
         preOrderString =   new StringBuilder();
     }
 
-    public static void pre(TreeNode root) {
-        if (root == null) {
+    public static void pre(TreeNode root)
+    {
+        if (root == null)
+        {
             return;
         }
         preOrderString.append(root.getValue() + "   ");
@@ -21,27 +25,36 @@ public class MyBinarySearchTree {
         pre(root.getRight());
     }
 
-    public TreeNode addService(final TreeNode treeRoot, final int addValue) {
-        if (treeRoot == null) {
+    public TreeNode addService(final TreeNode treeRoot, final int addValue)
+    {
+        if (treeRoot == null)
+        {
             return new TreeNode(addValue);
         }
-        if (addValue < treeRoot.getValue()) {
+        if (addValue < treeRoot.getValue())
+        {
             treeRoot.setLeft(addService(treeRoot.getLeft(), addValue));
-        } else if (addValue == treeRoot.getValue()) {
+        } else if (addValue == treeRoot.getValue())
+        {
             return treeRoot;
-        } else {
+        } else
+            {
             treeRoot.setRight(addService(treeRoot.getRight(), addValue));
         }
 
         return treeRoot;
     }
 
-    public void add(final int value) {
+    public void add(final int value)
+    {
+
         this.root = addService(this.root, value);
     }
 
-    public void post(TreeNode root) {
-        if (root == null) {
+    public void post(TreeNode root)
+    {
+        if (root == null)
+        {
             return;
         }
         post(root.getLeft());
