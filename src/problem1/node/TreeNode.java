7dde35009a3142743e07d6ed17df3eac908084cb
prakhar agarwal <prakhar.agarwal1_cs18@gla.ac.in>
2020-03-28 18:34:03 +0530

    tree node

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -6,5 +6,45 @@
  */
 package problem1.node;
 
-public class TreeNode {
+public class TreeNode<T>
+{
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int value)
+    {
+        this.value  =     value;
+        this.left = this.right = null;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public void setValue(int value)
+    {
+        this.value = value;
+    }
+
+    public TreeNode getLeft()
+    {
+        return left;
+    }
+
+    public void setLeft(TreeNode left)
+    {
+        this.left = left;
+    }
+
+    public TreeNode getRight()
+    {
+        return right;
+    }
+
+    public void setRight(TreeNode right)
+    {
+        this.right = right;
+    }
 }
